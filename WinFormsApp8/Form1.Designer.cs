a85147a52897306ec9a9a1d3d1e755508135804b
Andruhano <andrey.a.mykhailenko@gmail.com>
2024-12-04 23:29:52 +0200

    Add project files.

---
+++ WinFormsApp8/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WinFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            colorDialog1 = new ColorDialog();
+            fontDialog1 = new FontDialog();
+            button1 = new Button();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlLight;
+            button1.ForeColor = Color.Crimson;
+            button1.Location = new Point(212, 120);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Color";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe Script", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.ForeColor = SystemColors.ControlText;
+            button2.Location = new Point(512, 120);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Font";
+            button2.TextAlign = ContentAlignment.TopCenter;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 171);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(776, 267);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "The quick brown fox jumps over the lazy dog";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ColorDialog colorDialog1;
+        private FontDialog fontDialog1;
+        private Button button1;
+        private Button button2;
+        private RichTextBox richTextBox1;
+    }
+}
